6beea46abccb6204b1139717607d79464389b849
Fabrizio Signoretta <signorettafabrizio@gmail.com>
2022-08-15 20:05:59 -0300

    carpeteando

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
-import { useState } from 'react'
+import { Class7 } from './class7/Class7'
 
 function App () {
-  const [count, setCount] = useState(0)
-  // useState => [estado, modificadorEstado()]
-  // muy facil lo de hoy, mejor practicamos useState 😎
-
-  function add () {
-    setCount(count + 1)
-  }
-  function decrement () {
-    if (count - 1 < 0) {
-      return
-    } else {
-      setCount(count - 1)
-    }
-  }
-  function reset () {
-    setCount(0)
-  }
-  return (
-    <div className='App'>
-      <h1>{count}</h1>
-      <button onClick={decrement}>-</button>
-      <button onClick={add}>+</button>
-      <button onClick={reset}>Reset</button>
-    </div>
-  )
+  return <Class7 />
 }
 
 export default App
